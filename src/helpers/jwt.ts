beb947679b13200baaf6d212b26ad950ba0e7125
Leonardo Avendaño <leo.aven94@gmail.com>
2021-07-11 21:41:43 -0300

    Added authentification middleware

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,6 +1,25 @@
 import * as jwt from 'jsonwebtoken';
 import { dotEnv } from '../config/dotEnv';
+import { Request, Response } from 'express';
 
 export const generateAccessToken = (userdata: string | object) => {
   return jwt.sign(userdata, dotEnv.secret, { expiresIn: '1800s' });
+}
+
+export const authenticateToken = (req: Request, res: Response, next: any) => {
+  
+  if (!req.headers.token) return res.status(401).send('Missing auth token');
+
+  const token = String(req.headers.token);
+
+  jwt.verify(token, dotEnv.secret, (err, user) => {
+    if (err) return res.status(403).send(err);
+    if (user) {
+      req.params.name = user.name;
+      req.params.email = user.email;
+      req.params.phone = user.phone;
+      req.params.avatar = user.avatar;
+    }
+    next();
+  });
 }
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,20 +1,24 @@
 import e, { Router } from 'express';
 import { productsController } from '../controllers/';
+import { authenticateToken } from '../helpers/jwt';
 
 const productsRouter: Router = e.Router();
 
 productsRouter.get('/', productsController.getProducts);
 productsRouter.get('/:id', productsController.getOneProduct);
 
-productsRouter.post('/', 
+productsRouter.post('/',
+  authenticateToken,
   e.json(),  
   productsController.addProducts);
 
-productsRouter.put('/:id', 
+productsRouter.put('/:id',
+  authenticateToken,
   e.json(),
   productsController.updateProduct);
 
-productsRouter.delete('/:id', 
+productsRouter.delete('/:id',
+  authenticateToken, 
   e.json(),
   productsController.deleteOneProduct);
 
